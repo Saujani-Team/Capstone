5e22338c025a52b315b0bd1609b9e9507bf0e50e
Allie Bug <hw600@nyu.edu>
2022-12-07 10:44:21 -0500

    styles

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -6,26 +6,28 @@ import { logout } from "../store";
 const Navbar = ({ handleClick, isLoggedIn, auth }) => (
   <div>
     <h1>Draw Your Face Off</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <Link to="/draw">Draw</Link>
-          <Link to={`/users/${auth.id}`}>My Profile</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/home">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/draw">Draw</Link>
-        </div>
-      )}
+    <nav class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
+      <div class="container-fluid">
+        {isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links after you log in */}
+            <Link to="/home">Home</Link>
+            <Link to="/draw">Draw</Link>
+            <Link to={`/users/${auth.id}`}>My Profile</Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/home">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/draw">Draw</Link>
+          </div>
+        )}
+      </div>
     </nav>
     <hr />
   </div>
