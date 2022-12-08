7a9d7d99667a7cad4f9f9c2c5f162864090b0586
Ilana Bye <ilanagbye@gmail.com>
2022-12-08 10:14:14 -0700

    add name logo

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,15 +2,19 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
+// import DYFO from "../../public/DYFO.png";
+
+// const DYFO = require("../../public/DYFO.png");
 
 const Navbar = ({ handleClick, isLoggedIn, auth }) => (
   <div>
-    <h1>Draw Your Face Off</h1>
     <nav>
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
+          <Link to="/home">
+            <img src={"/DYFO.png"} height={40} />
+          </Link>
           <Link to="/draw">Draw</Link>
           <Link to={`/users/${auth.id}`}>My Profile</Link>
           <a href="#" onClick={handleClick}>
@@ -20,7 +24,9 @@ const Navbar = ({ handleClick, isLoggedIn, auth }) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          <Link to="/home">Home</Link>
+          <Link to="/home">
+            <img src={"/DYFO.png"} height={40} />
+          </Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
           <Link to="/draw">Draw</Link>
