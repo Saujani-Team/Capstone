0c1545a41c659b541f33fe8708e303c7e459a14c
Ilana Bye <ilanagbye@gmail.com>
2022-12-09 09:34:26 -0700

    home page updates

--- client/components/Home.js
+++ client/components/Home.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
 
 export const Home = (props) => {
-  const { username } = props;
+  console.log("props", props.auth);
+  const { firstName } = props;
   const { isLoggedIn } = props;
 
   return (
@@ -70,13 +71,20 @@ export const Home = (props) => {
         </Carousel.Item>
       </Carousel>
       {isLoggedIn ? (
-        <div>
-          <h3>Welcome, {username}!</h3>
+        <div className="home-text">
+          <h3>
+            <img src={"/Welcome.png"} height={40} /> {firstName}!
+          </h3>
           <h2>How to Use Draw Your Face Off!</h2>
-          <h3>Start a Drawing!</h3>
-          <p>Click Draw and Start Drawing to open your canvas. </p>
+          {/* <h3>Start a Drawing!</h3> */}
+          <p>
+            Click Draw and Start Drawing to open your canvas and start drawing.{" "}
+          </p>
           <h3>Invite Others!</h3>
-          <p>Click Generate Link 🖇️ and send the link to your collaborators.</p>
+          <p>
+            Click on Draw and Generate Link 🖇️ and send the link to your
+            collaborators.
+          </p>
           <h3>Save Drawings!</h3>
           <p>Click Save Drawing to have your drawing saved to your Profile.</p>
           <h3>My Profile!</h3>
@@ -89,17 +97,20 @@ export const Home = (props) => {
           </p>
         </div>
       ) : (
-        <div>
-          <h1>Welcome to DYFO!</h1>
-          <h2>How to Use Draw Your Face Off!</h2>
+        <div className="home-text">
+          <img src={"/WelcomeToDYFO.png"} height={40} />
           <h3>
-            You can <Link to="/login">Login,</Link>{" "}
-            <Link to="/signup">Sign Up,</Link> or start{" "}
+            You can get started by <Link to="/login">Logging in,</Link>{" "}
+            <Link to="/signup">Signing Up,</Link> or start{" "}
             <Link to="/draw">Drawing</Link> as a Guest!
           </h3>
-          <p>Click Start Drawing to open your canvas. </p>
+          <h2>How to Use Draw Your Face Off!</h2>
+          <p>Click on Draw and Start Drawing to open your canvas. </p>
           <h3>Invite Others!</h3>
-          <p>Click Generate Link 🖇️ and send the link to your collaborators.</p>
+          <p>
+            Click on Draw and Generate Link 🖇️ and send the link to your
+            collaborators.
+          </p>
         </div>
       )}
     </div>
@@ -112,7 +123,7 @@ export const Home = (props) => {
 const mapState = (state) => {
   return {
     isLoggedIn: !!state.auth.id,
-    username: state.auth.username,
+    firstName: state.auth.firstName,
     auth: state.auth,
   };
 };
